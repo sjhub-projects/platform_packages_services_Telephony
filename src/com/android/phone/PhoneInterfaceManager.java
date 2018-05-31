d1b82a045500372088e40992200267270abee22b
iamsj7 <shaikjaleel270@gmail.com>
2018-05-31 18:34:35 +0000

    Revert "Telephony: Add getLteOnGsmMode() method (3/3)"
    This reverts commit 2362a792981e36fe71a6cae44a90588937b2fbb3.

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -2376,10 +2376,6 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
         }
     }
 
-    public int getLteOnGsmMode() {
-        return mPhone.getLteOnGsmMode();
-    }
-
     public void setPhone(Phone phone) {
         mPhone = phone;
     }
